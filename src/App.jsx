993d100e91c34cb6297c910cc1e248f468550103
Johnny <johnny0929560027@gmail.com>
2025-06-28 17:10:08 +0800

    feat：tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-indigo-300">Hello, Mojito!</h1>
+    </div>
+  );
 };
 
 export default App;
