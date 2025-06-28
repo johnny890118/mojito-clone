de28a12b5ba95ebd1988a7135eef0095852a36ca
Johnny <johnny0929560027@gmail.com>
2025-06-28 18:09:09 +0800

    feat : 新增navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-indigo-300">Hello, Mojito!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
